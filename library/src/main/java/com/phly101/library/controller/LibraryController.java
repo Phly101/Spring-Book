1e3cb4368b6b3054b72912aa694f5c407ef4ebf8
PHLY101 <basel6889@gmail.com>
2026-07-20 00:48:21 +0300

    simplified the controller so it only handles the endpoint calls rather than checking everywhere

--- library/src/main/java/com/phly101/library/controller/LibraryController.java
+++ library/src/main/java/com/phly101/library/controller/LibraryController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.Optional;
 
 
 @RestController
@@ -33,8 +32,7 @@ public class LibraryController {
 
     @GetMapping("/books/{isbn}")
     public ResponseEntity<Book> findBookById(@PathVariable("isbn") String isbn) {
-        return libraryService.findBookByIsbn(isbn).map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+        return libraryService.findBookByIsbn(isbn).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
 
 
     }
@@ -42,9 +40,6 @@ public class LibraryController {
 
     @PostMapping("/members")
     public ResponseEntity<Member> createMember(@RequestBody CreateMemberRequest createMemberRequest) {
-        if (libraryService.findMemberById(createMemberRequest.memberId()).isPresent()) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        }
         switch (createMemberRequest.type()) {
             case STUDENT -> {
                 Student student = new Student(createMemberRequest.name(), createMemberRequest.memberId());
@@ -68,24 +63,10 @@ public class LibraryController {
 
     @PostMapping("/loans")
     public ResponseEntity<LoanResponse> createLoans(@RequestBody CreateLoanRequest createLoanRequest) {
+        Loan loan = libraryService.borrowBook(createLoanRequest.memberId(), createLoanRequest.isbn());
+        LoanResponse loanResponse = new LoanResponse(loan.getBook().getTitle(), loan.getMember().getMemberId(), loan.getLoanDate(), loan.getDueDate());
+        return ResponseEntity.status(HttpStatus.CREATED).body(loanResponse);
 
-        Member currentMember = libraryService.findMemberById(createLoanRequest.memberId()).orElse(null);
-        Book currentBook = libraryService.findBookByIsbn(createLoanRequest.isbn()).orElse(null);
-        if (currentBook == null || currentMember == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        } else {
-            Optional<Loan> loanResult = libraryService.borrowBook(currentMember, currentBook);
-            if (loanResult.isEmpty()) {
-                return ResponseEntity.status(HttpStatus.CONFLICT).build();
-            }
-            Loan loan = loanResult.get();
-            LoanResponse loanResponse = new LoanResponse(
-                    loan.getBook().getTitle(),
-                    loan.getMember().getMemberId(),
-                    loan.getLoanDate(),
-                    loan.getDueDate());
-            return ResponseEntity.status(HttpStatus.CREATED).body(loanResponse);
-        }
     }
 
     @GetMapping("/transactions/count")
@@ -96,14 +77,8 @@ public class LibraryController {
 
     @DeleteMapping("/loans/{isbn}")
     public ResponseEntity<Void> deleteBookLoan(@PathVariable("isbn") String isbn) {
-        Book book = libraryService.findBookByIsbn(isbn).orElse(null);
-        if (book == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-        if (libraryService.returnBook(book)) {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        libraryService.returnBook(isbn);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
