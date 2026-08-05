68491718edc69efdf11d99895f6055fcf0adfe7f
PHLY101 <basel6889@gmail.com>
2026-08-05 21:57:23 +0300

    added a 2 query functions

--- library/src/main/java/com/phly101/library/repository/LoanRepository.java
+++ library/src/main/java/com/phly101/library/repository/LoanRepository.java
@@ -3,10 +3,11 @@ package com.phly101.library.repository;
 import com.phly101.library.model.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface LoanRepository extends JpaRepository<Loan, UUID> {
     Optional<Loan> findByMemberMemberIdAndBookIsbnAndReturnDateIsNull(String memberId, String isbn);
+    boolean existsByMemberMemberIdAndReturnDateIsNull(String memberId);
+    boolean existsByBookIsbnAndReturnDateIsNull(String isbn);
 }
