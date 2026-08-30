c95fc5b87625ff5a7fdc18a7413885bb2d492afb
PHLY101 <basel6889@gmail.com>
2026-08-30 20:32:00 +0300

    added a find all by isbn function to the repo

--- library/src/main/java/com/phly101/library/repository/BookRepository.java
+++ library/src/main/java/com/phly101/library/repository/BookRepository.java
@@ -3,9 +3,12 @@ package com.phly101.library.repository;
 import com.phly101.library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
     Optional<Book> findByIsbn(String isbn);
+
+    List<Book> findByIsbnIn(List<String> isbn);
 }
