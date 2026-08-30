81457305b550ac0c331b2d77ef79f8c098c1ca4b
PHLY101 <basel6889@gmail.com>
2026-08-30 20:31:36 +0300

    added 2 new map functions for the list of books

--- library/src/main/java/com/phly101/library/mapper/BookMapper.java
+++ library/src/main/java/com/phly101/library/mapper/BookMapper.java
@@ -4,12 +4,17 @@ import com.phly101.library.dto.book.BookResponse;
 import com.phly101.library.dto.book.CreateBookRequest;
 import com.phly101.library.model.Book;
 
+import java.util.List;
+
 public class BookMapper {
     public static BookResponse toBookResponse(Book book) {
         return new BookResponse(
                 book.getTitle(),
                 book.getAuthor(),
-                book.getIsbn()
+                book.getIsbn(),
+                book.getPublishDate(),
+                book.getNumberOfPages(),
+                book.getCoverImage()
         );
     }
 
@@ -17,7 +22,31 @@ public class BookMapper {
         return new Book(
                 request.title(),
                 request.author(),
-                request.isbn()
+                request.isbn(),
+                request.publish_date(),
+                request.number_of_pages(),
+                request.cover_image()
         );
+
     }
+
+    public static List<Book> toEntities(List<CreateBookRequest> requests) {
+        return requests.stream()
+                .map(BookMapper::toEntity)
+                .toList();
+    }
+
+    public static List<BookResponse> toBooksResponse(List<Book> books) {
+        return books.stream()
+                .map(book -> new BookResponse(
+                        book.getTitle(),
+                        book.getAuthor(),
+                        book.getIsbn(),
+                        book.getPublishDate(),
+                        book.getNumberOfPages(),
+                        book.getCoverImage()
+                ))
+                .toList();
+    }
+
 }
